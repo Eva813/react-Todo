0f9d1718101961c56564c264763465bdb8d811d8
Eva813 <as45986@gmail.com>
2022-08-14 16:43:07 +0800

    feat: basic layout and input add list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import './all.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState, useEffect } from 'react';
+
+const initialList = [
+  {
+    id: '1',
+    name: 'Reading',
+  },
+  {
+    id: '2',
+    name: 'Shopping',
+  },
+];
 
 function App() {
+
+  //item list
+
+  const [list, setList] = useState(initialList);
+  const [item, setItem] = useState('');
+  function handleChange(e) {
+    setItem(e.target.value)
+  }
+  function handleAdd() {
+    // add item
+    const newList = [...list, { id: String(list.length + 1), name: item }]
+    console.log('list', newList)
+    setList(newList)
+    setItem('')
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="todoListPage" className="bg-half">
+
+      <h1><a href="#">ONLINE TODO LIST</a></h1>
+      <div className="conatiner todoListPage vhContainer">
+        <div className="todoList_Content">
+          <div className="inputBox">
+            <input type="text" placeholder="Please input todo " onChange={handleChange} value={item} />
+            <a onClick={handleAdd}>
+              <FontAwesomeIcon icon={['fa', 'plus']} />
+            </a>
+          </div>
+          <div className="todoList_items">
+
+            {/* 項目列表 */}
+            <ul className="todoList_item">
+              {list.map((item) => (
+                <li key={item.id}>
+                  <label className="todoList_label">
+                    <input className="todoList_input" type="checkbox" value="true" />
+                    <span>{item.name}</span>
+                  </label>
+                  <a href="#">
+                    <FontAwesomeIcon icon={['fa', 'times']} />
+                  </a>
+                </li>
+              ))}
+              {/* <li>
+                <label className="todoList_label">
+                  <input className="todoList_input" type="checkbox" value="true" />
+                  <span>把冰箱發霉的檸檬拿去丟</span>
+                </label>
+                <a href="#">
+                  <FontAwesomeIcon icon={['fa', 'times']} />
+                </a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+library.add(fas)
 root.render(
   <React.StrictMode>
     <App />
