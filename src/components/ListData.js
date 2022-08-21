b32feaff55cae0dc9060dfdf3d13a052b4bfe34e
Eva813 <as45986@gmail.com>
2022-08-21 20:05:27 +0800

    fix:set commponent of list and items

--- src/components/ListData.js
+++ src/components/ListData.js
@@ -1,56 +1,56 @@
-//包住整個items
-import React, { useEffect, useState } from 'react';
+// //包住整個items
+// import React, { useEffect, useState } from 'react';
 
-import ListItem from './ListItem';
-import TabBar from './TabBar';
+// import ListItem from './ListItem';
+// import TabBar from './TabBar';
 
-const sliceData = (data, currentPageIndex = 0) => {
-  console.log('slice-data-currentPageIndex:', data, currentPageIndex);
+// const sliceData = (data, currentPageIndex = 0) => {
+//   console.log('slice-data-currentPageIndex:', data, currentPageIndex);
 
-  return [...data].slice(currentPageIndex, currentPageIndex + 2);
-};
+//   return [...data].slice(currentPageIndex, currentPageIndex + 2);
+// };
 
-function ListData(props) {
-  const { data } = props;
-  console.log('data', data)
-  const tabName = ['all', 'uncompleted', 'completed']
-  //點擊分頁
+// function ListData(props) {
+//   const { data } = props;
+//   console.log('data', data)
+//   const tabName = ['all', 'uncompleted', 'completed']
+//   //點擊分頁
 
-  const [clickPage, setClickPage] = useState(0);
-  const [partData, setPartData] = useState([]);
-
-
-  useEffect(() => {
-    const currentPageIndex = clickPage * 2;
-
-    setPartData(() => {
-      return sliceData(data, currentPageIndex);
-    });
-
-  }, [data, clickPage]);
-
-  return (
-    <div className="conatiner todoListPage vhContainer">
-      <div className="todoList_list">
-        <nav>
-          <TabBar
-            dataCount={data.length}
-            setClickPage={setClickPage}
-          // totalPages={totalPages}
-          />
-        </nav>
-      </div>
-
-      {/* <ul className="py-2">
-        {partData.map((item) => {
-          return <ListItem key={item.id} item={item} />;
-        })}
-      </ul> */}
+//   const [clickPage, setClickPage] = useState(0);
+//   const [partData, setPartData] = useState([]);
 
 
-    </div>
-  );
+//   useEffect(() => {
+//     const currentPageIndex = clickPage * 2;
 
-}
+//     setPartData(() => {
+//       return sliceData(data, currentPageIndex);
+//     });
 
-export default ListData;
+//   }, [data, clickPage]);
+
+//   return (
+//     <div className="conatiner todoListPage vhContainer">
+//       <div className="todoList_list">
+//         <nav>
+//           <TabBar
+//             dataCount={data.length}
+//             setClickPage={setClickPage}
+//           // totalPages={totalPages}
+//           />
+//         </nav>
+//       </div>
+
+//       {/* <ul className="py-2">
+//         {partData.map((item) => {
+//           return <ListItem key={item.id} item={item} />;
+//         })}
+//       </ul> */}
+
+
+//     </div>
+//   );
+
+// }
+
+// export default ListData;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
-//每一個項目列
-function ListItem(props) {
-  // function ListItem({ item: { name } }) {
-  const {
-    item: { id, name },
-  } = props;
+// //每一個項目列
+// function ListItem(props) {
+//   // function ListItem({ item: { name } }) {
+//   const {
+//     item: { id, name },
+//   } = props;
 
-  return (
-    <li>
-      <p>{id}</p>
-      <p>{name}</p>
-    </li>
-  );
-}
-/* end of ListItem() */
+//   return (
+//     <li>
+//       <p>{id}</p>
+//       <p>{name}</p>
+//     </li>
+//   );
+// }
+// /* end of ListItem() */
 
-export default ListItem;
+// export default ListItem;
