234f560e68549e6e42e8320be194be4c39648e0d
Eva813 <as45986@gmail.com>
2022-08-21 20:04:42 +0800

    fix:modify Tabs to let todo list show

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,60 +1,97 @@
 import React, { useState, useEffect } from "react";
 import './all.css'
+import TodoList from './components/TodoList'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRoutes, Outlet, Link } from "react-router-dom"
+
+
+const initialList = [
+  {
+    id: '1',
+    name: 'Reading',
+    isComplete: true,
+  },
+  {
+    id: '2',
+    name: 'Shopping',
+    isComplete: true,
+  },
+  {
+    id: '3',
+    name: 'See the doctor',
+    isComplete: false,
+  },
+];
 
 const Tabs = () => {
-  const [activeIndex, setActiveIndex] = useState(1);
-  const handleClick = (index) => {
-    setActiveIndex(index)
-  }
-  const checkActive = (index, className) => activeIndex === index ? className : "";
+  // console.log('prop', props)
+  // const { list: todoList } = props
 
-  //check index to show active
+  // console.log('prop', todoList)
+
+
+  const [list, setList] = useState(initialList);
+  const [item, setItem] = useState('');
+  const [completed, setCompleted] = useState();
+
+
+  const [currentTab, setCurrentTab] = useState('all');
+
+
   useEffect(() => {
     const tab = document.querySelector('.todoList_tab');
-    console.log('tab', tab)
     const aTags = tab.querySelectorAll('a');
-    console.log('aTags', aTags)
-    aTags.forEach((item, index) => {
-      console.log('item', item, index)
-      if (index + 1 === activeIndex) {
 
+    aTags.forEach((atag) => {
+      if (atag.id == currentTab) {
+        atag.classList.add('active');
+      } else {
+        atag.classList.remove('active');
       }
     })
+  }, [currentTab])
 
-  }, [activeIndex])
+  const handleTab = (e) => {
+    setCurrentTab(e.target.id);
+  }
+
+
+
+  function handleChange(e) {
+    setItem(e.target.value)
+  }
+  function handleAdd() {
+    // add item
+    const newList = [...list, { id: String(list.length + 1), name: item, isComplete: false, }]
+    console.log('list', newList)
+    setList(newList)
+    setItem('')
+  }
 
   return (
     <>
-      <div className="todoList_list">
-        <ul className="todoList_tab">
-          <li><a href="#" className={checkActive(1, "active")} onClick={() => handleClick(1)}>全部</a></li>
-          <li><a href="#" className={checkActive(2, "active")} onClick={() => handleClick(2)}>待完成</a></li>
-          <li><a href="#" className={checkActive(3, "active")} onClick={() => handleClick(3)}>已完成</a></li>
-        </ul>
-        <div className="todoList_items">
-          <div className={checkActive(1, "active")}>
-            <ul className="todoList_item">
-              <li>
-                I AM ONE
-              </li>
-            </ul>
+      <div className="conatiner todoListPage vhContainer">
+        <div className="todoList_Content">
+          <div className="inputBox">
+            <input type="text" placeholder="Please input todo " onChange={handleChange} value={item} />
+            <a onClick={handleAdd}>
+              <FontAwesomeIcon icon={['fa', 'plus']} />
+            </a>
           </div>
-          <div className={checkActive(2, "active")}>
-            <ul className="todoList_item">
-              <li>
-                I AM Two
-              </li>
+          <div className="todoList_list">
+            <ul className="todoList_tab">
+              <li><a as={Link} to="/Tabs" id="all" onClick={handleTab}>全部</a></li>
+              <li><a as={Link} to="/Tabs" id='unfinished' onClick={handleTab}>待完成</a></li>
+              <li><a as={Link} to="/Tabs" id='finished' onClick={handleTab}>已完成</a></li>
             </ul>
-          </div>
-          <div className={checkActive(3, "active")}>
-            <ul className="todoList_item">
-              <li>
-                I AM THree
-              </li>
-            </ul>
+            <TodoList list={list} currentTab={currentTab} />
+            {/* <div className="todoList_items">
+
+        </div> */}
           </div>
         </div>
       </div>
+
     </>
   )
 }
