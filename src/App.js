466110933e7f95b95296e675dff891cee6a2a556
Eva813 <as45986@gmail.com>
2022-08-21 20:04:10 +0800

    fix:import useRoute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 //import logo from './logo.svg';
 //import './App.css';
 import './all.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState, useEffect } from 'react';
 import Tabs from './Tabs'
-import TabBar from './components/TabBar'
-import ListData from './components/ListData'
+import Layout from './components/Layout'
+// import TabBar from './components/TabBar'
+// import ListData from './components/ListData'
+import { Routes, Route, Link, useRoutes } from "react-router-dom";
+import NotFound from "./components/NotFound"
+import routes from "./routes"
+
+
+
+// App.js
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+
+
 
 const initialList = [
   {
@@ -26,17 +66,20 @@ const initialList = [
 ];
 
 function App() {
-
+  const elements = useRoutes(routes);
+  return elements
   //item list
 
   const [list, setList] = useState(initialList);
   const [item, setItem] = useState('');
+  const [completed, setCompleted] = useState();
+
   function handleChange(e) {
     setItem(e.target.value)
   }
   function handleAdd() {
     // add item
-    const newList = [...list, { id: String(list.length + 1), name: item }]
+    const newList = [...list, { id: String(list.length + 1), name: item, isComplete: false, }]
     console.log('list', newList)
     setList(newList)
     setItem('')
@@ -48,52 +91,42 @@ function App() {
 
     <div id="todoListPage" className="bg-half">
 
-      <h1><a href="#">ONLINE TODO LIST</a></h1>
-      <div className="conatiner todoListPage vhContainer">
-        <div className="todoList_Content">
-          <div className="inputBox">
+
+      {/* <div className="conatiner todoListPage vhContainer">
+        <div className="todoList_Content"> */}
+      {/* <div className="inputBox">
             <input type="text" placeholder="Please input todo " onChange={handleChange} value={item} />
             <a onClick={handleAdd}>
               <FontAwesomeIcon icon={['fa', 'plus']} />
             </a>
-          </div>
-          {/* <Tabs /> */}
-          {/* <TabBar
+          </div> */}
+      {/* <Tabs list={list} /> */}
+      {/* <TabBar
             dataCount={data.length}
             setClickPage={setClickPage}
           // totalPages={totalPages}
           /> */}
 
-          <ListData data={initialList} />
-          <div className="todoList_items">
+      {/* <h1>Welcome to React Router!</h1> */}
+      {/* <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="tabs" element={<Tabs list={list} />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      
+      </Routes> */}
 
-            {/* 項目列表 */}
-            <ul className="todoList_item">
-              {list.map((item) => (
-                <li key={item.id}>
-                  <label className="todoList_label">
-                    <input className="todoList_input" type="checkbox" value="true" />
-                    <span>{item.name}</span>
-                  </label>
-                  <a href="#">
-                    <FontAwesomeIcon icon={['fa', 'times']} />
-                  </a>
-                </li>
-              ))}
-              {/* <li>
-                <label className="todoList_label">
-                  <input className="todoList_input" type="checkbox" value="true" />
-                  <span>把冰箱發霉的檸檬拿去丟</span>
-                </label>
-                <a href="#">
-                  <FontAwesomeIcon icon={['fa', 'times']} />
-                </a>
-              </li> */}
-            </ul>
-          </div>
-        </div>
+
+      {/* <ListData data={initialList} /> */}
+      <div className="todoList_items">
+
+        {/* 項目列表 */}
+
       </div>
+      {/* </div>
+      </div> */}
     </div>
+
   );
 }
 
