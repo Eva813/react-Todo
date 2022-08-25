45a5c063660e2153231dfc9a97dc469cbdce95bf
Eva813 <as45986@gmail.com>
2022-08-25 21:02:43 +0800

    feat:add signIn,signUp Page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,7 +20,7 @@ const Layout = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link as={Link} to="/Login" >Login</Nav.Link>
+                <Nav.Link as={Link} to="/SignIn" >Login</Nav.Link>
                 <Nav.Link as={Link} to="/Tabs">TodoList</Nav.Link>
                 {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { useRoutes } from "react-router-dom";
 import Tabs from "../Tabs"
-import Login from "../components/Login"
+import SignIn from "../components/SignIn"
+import SignUp from "../components/SignUp"
 import Layout from "../components/Layout"
 
 
@@ -14,7 +15,9 @@ let routes = [
         path: "/tabs",
         element: <Tabs />,
       },
-      { path: "login", element: <Login /> },
+      { path: "signUp", element: <SignUp /> },
+      { path: "signIn", element: <SignIn /> },
+
     ],
   },
   // { path: "team", element: <AboutPage /> },
