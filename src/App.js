04fe6ee9c2fdb4ab51e6599f11d6e7e9ea377673
Eva813 <as45986@gmail.com>
2022-09-03 14:04:52 +0800

    delete no use components ,modify layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 //import './App.css';
 import './all.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Tabs from './Tabs'
 import Layout from './components/Layout'
 import SignIn from "./components/SignIn"
@@ -10,67 +10,11 @@ import { AuthContext } from "./components/Context";
 import { useAuth } from './components/Context';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
-
-// import TabBar from './components/TabBar'
-// import ListData from './components/ListData'
 import { Routes, Route, Link, useRoutes } from "react-router-dom";
 import NotFound from "./components/NotFound"
-import routes from "./routes"
+// import routes from "./routes"
 
 
-
-// App.js
-// function Home() {
-//   return (
-//     <>
-//       <main>
-//         <h2>Welcome to the homepage!</h2>
-//         <p>You can do this, I believe in you.</p>
-//       </main>
-//       <nav>
-//         <Link to="/about">About</Link>
-//       </nav>
-//     </>
-//   );
-// }
-
-// function About() {
-//   return (
-//     <>
-//       <main>
-//         <h2>Who are we?</h2>
-//         <p>
-//           That feels like an existential question, don't you
-//           think?
-//         </p>
-//       </main>
-//       <nav>
-//         <Link to="/">Home</Link>
-//       </nav>
-//     </>
-//   );
-// }
-
-
-
-
-const initialList = [
-  {
-    id: '1',
-    name: 'Reading',
-    isComplete: true,
-  },
-  {
-    id: '2',
-    name: 'Shopping',
-    isComplete: true,
-  },
-  {
-    id: '3',
-    name: 'See the doctor',
-    isComplete: false,
-  },
-];
 
 function App() {
   // const elements = useRoutes(routes);
@@ -86,6 +30,7 @@ function App() {
       <AuthContext.Provider value={{ token, setToken }}>
         <Routes>
           <Route path="/" element={<Layout />}>
+            <Route path="/" element={<SignIn />} />
             <Route path="/SignIn" element={<SignIn />} />
             <Route element={<ProtectedRoute />}>
               <Route path="/tabs" element={<Tabs />} />
@@ -97,43 +42,6 @@ function App() {
       </AuthContext.Provider>
     </div>
 
-    // <div id="todoListPage" className="bg-half">
-
-
-    //   {/* <div className="conatiner todoListPage vhContainer">
-    //     <div className="todoList_Content"> */}
-    //   {/* <div className="inputBox">
-    //         <input type="text" placeholder="Please input todo " onChange={handleChange} value={item} />
-    //         <a onClick={handleAdd}>
-    //           <FontAwesomeIcon icon={['fa', 'plus']} />
-    //         </a>
-    //       </div> */}
-    //   {/* <Tabs list={list} /> */}
-    //   {/* <TabBar
-    //         dataCount={data.length}
-    //         setClickPage={setClickPage}
-    //       // totalPages={totalPages}
-    //       /> */}
-
-    //   {/* <h1>Welcome to React Router!</h1> */}
-    //   {/* <Routes>
-    //     <Route path="/" element={<Layout />}>
-    //       <Route path="tabs" element={<Tabs list={list} />} />
-    //       <Route path="about" element={<About />} />
-    //     </Route>
-
-    //   </Routes> */}
-
-
-    //   {/* <ListData data={initialList} /> */}
-    //   <div className="todoList_items">
-
-    //     {/* 項目列表 */}
-
-    //   </div>
-    //   {/* </div>
-    //   </div> */}
-    // </div>
 
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,6 @@ import { useRoutes, Outlet, Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAuth } from "./Context";
 
 const Layout = () => {
@@ -23,22 +22,10 @@ const Layout = () => {
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/SignIn" >Login</Nav.Link>
                 <Nav.Link as={Link} to="/Tabs">TodoList</Nav.Link>
-                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown> */}
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-
 
         <main>
 
